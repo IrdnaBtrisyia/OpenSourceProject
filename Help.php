a618a01b5b8b4e94fef82d330a6db85c38cd3093
Irdina Batrisyia <80381951+IrdnaBtrisyia@users.noreply.github.com>
2021-04-21 11:55:45 +0800

    Add files via upload

---
+++ Help.php
@@ -0,0 +1,26 @@
+<HTML>
+<body>
+
+<?php
+
+	$input = $_POST ['name'];
+	
+?>
+
+<p style="text-align: center;"><img src="https://cdn.jotfor.ms/img/check-icon.png" alt="" width="128" height="128" /></p>
+<div style="text-align: center;">
+<h1 style="text-align: center;">Thank You!</h1>
+<p>
+
+<?php
+Print "$input";
+?>
+
+</p>
+<p style="text-align: center;">Your message has been sent. We will be with you in a sec. </p>
+</div>
+
+
+
+</body>
+</HTML>
